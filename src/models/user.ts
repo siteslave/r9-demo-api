f69447e9440902d7f8c22d21cbfe7d10489929cb
Satit Rianpit <rianpit@gmail.com>
2017-05-24 13:21:00 +0700

    success login with md5

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,7 +1,7 @@
 import Knex = require('knex');
 
 export class UserModel {
-  doLogin(knex: Knex, username: any, password: any) {
+  doLogin(knex: Knex, username: string, password: string) {
     return knex('users')
       .select('id', 'username', 'fullname')
       .where({
@@ -9,4 +9,11 @@ export class UserModel {
         password: password
       });
   }
+}
+
+export interface IUser {
+  username?: string;
+  passsword?: string;
+  id?: string;
+  fullname?: string;
 }
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,11 @@
 'use strict';
 
 import * as express from 'express';
-import { UserModel } from '../models/user';
+import * as crypto from 'crypto';
+
+import { UserModel, IUser } from '../models/user';
 
 const router = express.Router();
-
 const userModel = new UserModel();
 
 router.post('/',(req, res, next) => {
@@ -12,9 +13,22 @@ router.post('/',(req, res, next) => {
   let password = req.body.password;
   let db = req.db;
 
-  userModel.doLogin(db, username, password)
+  let encPassword = crypto.createHash('md5')
+    .update(password)
+    .digest('hex');
+
+  userModel.doLogin(db, username, encPassword)
     .then((rows) => {
-      res.send({ ok: true, rows: rows });
+      // res.send({ ok: true, rows: rows });
+      if (rows.length) { // success
+        let username = rows[0].username;
+        let id = rows[0].id;
+        let fullname = rows[0].fullname;
+
+        res.send({ ok: true, token: 'xxxxxxx', fullname: fullname, id: id });
+      } else { // failed
+        res.send({ ok: false, error: 'ชื่อผู้ใช้งาน/รหัสผ่าน ไม่ถูกต้อง' });
+      }
     })
     .catch((error) => {
       console.log(error);
