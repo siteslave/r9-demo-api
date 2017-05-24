1d9302ee6b9a9727ea573a9c32f7d6800d43d4e2
Satit Rianpit <rianpit@gmail.com>
2017-05-24 16:11:04 +0700

    add token

---
+++ src/models/jwt.ts
@@ -0,0 +1,21 @@
+import * as jwt from 'jsonwebtoken';
+
+export class Jwt {
+
+  secretKey = '123456789000001234567890';
+
+  singn(payload: any) {
+    return new Promise((resolve, reject) => {
+      jwt.sign(payload, this.secretKey, {
+        expiresIn: '1d'
+      }, (error, token) => {
+        if (error) reject(error);
+        else resolve(token);
+      });
+    });
+  }
+
+  verify() {
+
+  }
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,22 @@
 import * as express from 'express';
 const router = express.Router();
 
+import { Jwt } from '../models/jwt';
+
+const jwt = new Jwt();
 /* GET home page. */
 router.get('/',(req,res,next) => {
   res.send({ ok: true, message: 'Welcome to my service!' });
+});
+
+router.get('/gen-token',(req,res,next) => {
+  jwt.singn({ name: 'Satit Rianpit' })
+    .then((token: any) => {
+      res.send({ ok: true, token: token });
+    })
+    .catch((error: any) => {
+      res.send({ ok: false, error: error.message });
+    });
 });
 
 router.get('/hello',(req,res,next) => {
